5e425ead432f85e904c39c985e127600165b53a9
mfxjx000205 <mfxjx000205@163.com>
2024-11-23 19:49:10 +0800

    Add FRAM driver and test, FRAM can R/W single and multi bytes.

---
+++ Athena-Driver/BSP/Components/FM25CL64/src/fm25_platform.c
@@ -0,0 +1,38 @@
+/*
+ * fm25_platform.c
+ *
+ *  Created on: Nov 23, 2024
+ *      Author: Hanjie Yu
+ */
+
+#include "fm25_platform.h"
+#include "fm25cl64.h"
+#include "spi_drv.h"
+#include "main.h"
+#define SPI_BUFFER_MAX_SIZE 240
+static uint8_t spiTxBuffer[SPI_BUFFER_MAX_SIZE];
+static uint8_t spiRxBuffer[SPI_BUFFER_MAX_SIZE];
+/*读FM25寄存器值*/
+void ReadDataFromFM25(uint8_t *rData, uint16_t rSize) {
+	spiBeginTransaction();
+	spi2Exchange(SPI2, rSize, spiTxBuffer, rData);
+	spiEndTransaction();
+
+}
+
+/*写FM25寄存器值*/
+void WriteDataToFM25(uint8_t *wData, uint16_t wSize) {
+	spiBeginTransaction();
+	spi2Exchange(SPI2, wSize, wData, spiRxBuffer);
+	spiEndTransaction();
+}
+
+/*片选操作*/
+void ChipSelectForFM25(FM25CSType cs) {
+	if (cs == FM25CS_Enable) {
+		LL_GPIO_ResetOutputPin(FM25CLXX_CS_GPIO_Port, FM25CLXX_CS_Pin);
+	} else {
+		LL_GPIO_SetOutputPin(FM25CLXX_CS_GPIO_Port, FM25CLXX_CS_Pin);
+	}
+}
+
