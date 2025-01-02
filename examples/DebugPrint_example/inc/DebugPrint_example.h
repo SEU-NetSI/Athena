3effbb935fe83e2be548d241f7e6595880d0c27c
eurkaaaa <13651011196@qq.com>
2025-01-02 20:38:11 +0800

    增加了debug print部分案例，成功测试

---
+++ examples/DebugPrint_example/inc/DebugPrint_example.h
@@ -0,0 +1,2 @@
+#include "debug.h"
+void Debug_example(void *arugment);
---
+++ examples/DebugPrint_example/src/DebugPrint_example.c
@@ -0,0 +1,17 @@
+#include "DebugPrint_example.h"
+void Debug_example(void *arugment)
+{
+    while(1)
+    {
+        vTaskDelay(2000);
+        //test print uint 
+        DEBUG_PRINTF("this is a test: %u \n", 85);
+        //test print float
+        DEBUG_PRINTF("this is a test: %.2f \n", 5.82);
+        //test print int
+        DEBUG_PRINTF("this is a test: %i \n", -19);
+        //test print string
+        DEBUG_PRINTF("this is a test: %s \n","hello world");
+    }
+
+}
