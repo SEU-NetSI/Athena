7467896bd58f4740fcd5cdfd253e3d320416a76f
hyx <hyx18234172491@163.com>
2025-01-03 22:13:20 +0800

    现在可以正常运行

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -43,7 +43,7 @@
 #include "../../examples/tinymap/inc/calibration.h"
 
 #include "../../examples/FS_example/src/Flash_FS_Example.c"
-//#include "uwb_send_recv_packet_example.c"
+#include "uwb_send_recv_packet_example.c"
 //#include "adhocuwb.h"
 
 /* USER CODE END Includes */
@@ -113,7 +113,7 @@ const osThreadAttr_t Debug_Example_attributes = {
 osThreadId_t uwb_send_recv_packet_ExampleHandle;
 const osThreadAttr_t uwb_send_recv_packet_Example_attributes = {
 		.name = "uwbSendRecvPacketTask",
-		.stack_size = 128 * 10,
+		.stack_size = 2 * UWB_FRAME_LEN_MAX * sizeof(StackType_t),
 		.priority = (osPriority_t) osPriorityNormal,
 };
 /* Private function prototypes -----------------------------------------------*/
@@ -133,67 +133,7 @@ void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
   *
   */
 
-void simpleTxCallback(void *argument) {	// 发送完数据包后的回调函数
-	return;
-}
 
-void simpleRxCallback(void *argument) {	// 接收到数据包时的回调函数
-	uint8_t *packet = (uint8_t *) argument;
-	return;
-}
-
-osThreadId_t uwbTaskHandle;
-const osThreadAttr_t uwbTask_attributes = {
-	  .name = "uwbTask",
-	  .stack_size = 5 * UWB_FRAME_LEN_MAX * sizeof(StackType_t), //TODO: check whether this works
-	  .priority = (osPriority_t) osPriorityNormal,
-	};
-osThreadId_t uwbISRTaskHandle;
-
-void initUWBConfig(){
-
-
-	// reset dw3000 chip
-	dwt_ops.reset(); // this is not necessary
-
-	// prepare the interrupt service routines task
-	uwbISRTaskHandle = osThreadNew(uwbISRTask, NULL, &uwbTask_attributes);
-	vTaskDelay(8); // wait for the uwbISRTask to start to handle ISR
-
-	// init the dw3000 chip, get ready to rx and rx，下面两次初始化是为了两个不同SPI配置的dw3000设备
-	int result = dw3000_init();
-	uint32_t dev_id = dwt_readdevid();
-	if (dev_id != 0x0 && dev_id != (0xDECA0302))
-	{
-	  MX_SPI2_Init_IO2IO3();
-	  dw3000_init();
-	}
-}
-
-
-
-void uwbSendRecvPacketTask(void *argument)
-{
-
-	initUWBConfig();
-
-	// set the tx and rx callback functions
-	adhocuwb_set_hdw_cbs(simpleTxCallback, simpleRxCallback);
-
-	// set the chip in listening mode, rxcallback should be invoked once a packet is received.
-	// you should see the RX led flashes at the UWB Deck
-	adhocuwb_hdw_force_rx();
-
-	/*============ the above code need only support from BSP/Components/DW3000 =============*/
-//	int uwbdata_tx[10] = {1,2,3,4,5,6,7,8,9};
-	int uwbdata_tx[2] = {1,2};
-	// loop forever
-	while(1)
-	{
-	  adhocuwb_hdw_send(uwbdata_tx, sizeof(uwbdata_tx));
-      vTaskDelay(2000);
-	}
-}
 
 void MX_FREERTOS_Init(void) {
   /* USER CODE BEGIN Init */
