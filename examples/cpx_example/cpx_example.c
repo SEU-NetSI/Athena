ba34f4e123185e22387141bb741762db27067431
wyb217 <1838798301@qq.com>
2025-02-26 20:30:21 +0800

    fix cpx bug and set stacksize to 700

--- examples/cpx_example/cpx_example.c
+++ examples/cpx_example/cpx_example.c
@@ -10,7 +10,7 @@
 osThreadId_t cpx_ExampleHandle;
 const osThreadAttr_t cpx_Example_attributes = {
 		.name = "cpx_Example",
-		.stack_size = 2000,
+		.stack_size = 700,
 		.priority = (osPriority_t) osPriorityNormal,
 };
 
@@ -37,7 +37,9 @@ static void cpx_Example(void *argument)
 	uint8_t count=0;
   while(1)
   {
+
 	    vTaskDelay(2000);
+	    int mark=uxTaskGetStackHighWaterMark(cpx_ExampleHandle);
 	    cpxInitRoute(CPX_T_GAP8, CPX_T_STM32, CPX_F_APP, &cpxPacket.route);
 	    cpxPacket.dataLength = 1;
 	    cpxPacket.data[0]=count;
@@ -55,4 +57,4 @@ static const UserInit cpx_init = {
 		.init = cpx_example_init,
 };
 
-//USER_INIT(cpx_init);
+USER_INIT(cpx_init);
