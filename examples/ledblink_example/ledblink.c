9dd9525459b55f423eb8c2ef4e2d6b98c4040df2
Feng Shan <twinhorse@twinhorse.net>
2025-03-13 18:36:03 +0800

    LED 1-6 all works

--- examples/ledblink_example/ledblink.c
+++ examples/ledblink_example/ledblink.c
@@ -28,21 +28,25 @@ static void ledTask(void *argument)
 {
 	  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
 	  LL_AHB4_GRP1_EnableClock(LL_AHB4_GRP1_PERIPH_GPIOC);
-	  LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_6);
-	  GPIO_InitStruct.Pin = LL_GPIO_PIN_6;
+	  GPIO_InitStruct.Pin = (LL_GPIO_PIN_6 | LL_GPIO_PIN_7);
 	  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
 	  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
 	  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
 	  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
 	  LL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
+	  GPIO_InitStruct.Pin = (LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12);
+	  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+	  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+	  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+	  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+	  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
   while(1)
   {
-	LL_GPIO_TogglePin(GPIOA, LL_GPIO_PIN_12);	//LED1
-	LL_GPIO_TogglePin(GPIOA, LL_GPIO_PIN_11);	//LED2
-	LL_GPIO_TogglePin(GPIOA, LL_GPIO_PIN_10);	//LED3
-	LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_7);	//LED4
-	LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_6);	//LED5
+	LL_GPIO_TogglePin(GPIOA, LL_GPIO_PIN_12);	//LED1, work correct
+	LL_GPIO_TogglePin(GPIOA, LL_GPIO_PIN_11);	//LED2, work correct
+	LL_GPIO_TogglePin(GPIOA, LL_GPIO_PIN_10);	//LED3, work correct
+	LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_7);	//LED4, work correct
+	LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_6);	//LED5, work correct
 	LL_GPIO_TogglePin(GPIOD, LL_GPIO_PIN_13);	//LED6, work correct
 
 	vTaskDelay(500);
