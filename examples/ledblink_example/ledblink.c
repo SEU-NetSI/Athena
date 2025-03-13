1e53268068ee1fa0b5ccf4c8fe359d917d43e17e
BrightUiv <19552605785@163.com>
2025-03-13 21:57:31 +0800

    整理led驱动

--- examples/ledblink_example/ledblink.c
+++ examples/ledblink_example/ledblink.c
@@ -26,31 +26,18 @@ USER_INIT(led_init);
 
 static void ledTask(void *argument)
 {
-	  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
-	  LL_AHB4_GRP1_EnableClock(LL_AHB4_GRP1_PERIPH_GPIOC);
-	  GPIO_InitStruct.Pin = (LL_GPIO_PIN_6 | LL_GPIO_PIN_7);
-	  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
-	  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
-	  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-	  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-	  LL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-	  GPIO_InitStruct.Pin = (LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12);
-	  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
-	  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
-	  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-	  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-	  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
 
 int ledcode=0;
 	  while(1)
 	  {
 		  vTaskDelay(100);
-		  LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_12);
-		  LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_11);
-		  LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_10);
-		  LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_7);
-		  LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_6);
-		  LL_GPIO_ResetOutputPin(GPIOD, LL_GPIO_PIN_13);
+//		  LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_12);
+//		  LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_11);
+//		  LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_10);
+//		  LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_7);
+//		  LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_6);
+//		  LL_GPIO_ResetOutputPin(GPIOD, LL_GPIO_PIN_13);放在gpio.c中进行初始化
 		  if(ledcode & (0x1UL << 0U))
 			  LL_GPIO_SetOutputPin(GPIOA, LL_GPIO_PIN_12);
 		  if(ledcode & (0x1UL << 1U))
