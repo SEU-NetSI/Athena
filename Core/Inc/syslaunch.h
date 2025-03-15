b1bf2d522a882a5c913f5c4ff730838121156393
Feng Shan <twinhorse@twinhorse.net>
2025-03-15 21:24:15 +0800

    add new files syslaunch.h and c

---
+++ Core/Inc/syslaunch.h
@@ -0,0 +1,15 @@
+/*
+ * syslaunch.h
+ *
+ *  Created on: Jan 15, 2025
+ *      Author: twinhorse
+ */
+
+#ifndef INC_SYSLAUNCH_H_
+#define INC_SYSLAUNCH_H_
+
+#define ENABLE_UWB_DW3000
+
+int getInitStatus();
+
+#endif /* INC_SYSLAUNCH_H_ */
